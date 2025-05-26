496101614e95c9f421969323b66f697f88520662
David Klehr <klehr@uni-trier.de>
2025-05-26 16:54:43 +0200

    Skript 1 angepasst

--- src/1_extract_pure.py
+++ src/1_extract_pure.py
@@ -1,55 +1,69 @@
-from osgeo import gdal
-from params_avg import params
+import geopandas as gpd
+import rasterio
+from rasterio.vrt import WarpedVRT
+from rasterio.windows import Window
+import numpy as np
+from tqdm import tqdm
 import os
+from params_2021_thermal_time import params
 
-def GetExtent(ds):
-    """ Return list of corner coordinates from a gdal Dataset """
-    xmin, xpixel, _, ymax, _, ypixel = ds.GetGeoTransform()
-    width, height = ds.RasterXSize, ds.RasterYSize
-    xmax = xmin + width * xpixel
-    ymin = ymax + height * ypixel
 
-    extent= [xmin, ymin, xmax, ymax]
-    extent_string = ''
-    for i in range(len(extent)):
-        if i < len(extent) - 1:
-            extent_string += str(extent[i]) + ' '
-        else:
-            extent_string += str(extent[i])
-    return extent_string
+bands = ['BLU', 'GRN', 'RED', 'RE1', 'RE2', 'RE3', 'BNIR', 'NIR', 'SW1', 'SW2']
+vrt_paths = {
+    band: f"/data/ahsoka/eocp/forestpulse/INTERNAL/spline/5day_interval/thermal/mosaic/stack_{band}.vrt"
+    for band in bands
+}
+    
+# 1. Lade Punkte
+gdf = gpd.read_file("/data/ahsoka/eocp/forestpulse/INTERNAL/BWI4/all_trainings_points.gpkg")
+gdf = gdf.to_crs("EPSG:3035")
 
-if __name__ == '__main__':
-    list_tile = os.listdir(params['DATA_CUBE_DIR'])
-    for tile in list_tile:
-        if tile == "mosaic":
-            print('mosiac skipped')
-            continue        
-        print(tile)
-        # Using blue band in 2022 to get the extent, change band or year if not available.
+arr_x = []
+arr_y = []
 
-        # original:
-        # blue_band_path = os.path.join(params['DATA_CUBE_DIR'], tile, '2017-2021_001-365_HL_TSA_SEN2L_BLU_RSP.tif')
-        first_year = int(params['YEAR_LIST'][0])-3
-        last_year = int(params['YEAR_LIST'][0])+1
-        blue_band_path = os.path.join(params['DATA_CUBE_DIR'], tile, '{y1}-{y2}_001-365_HL_TSA_SEN2L_BLU_RSP.tif'.format(y1=first_year, y2=last_year))
-        if not os.path.isfile(blue_band_path):
-            print('Not tile, skipping!')
-            print('Not tile, skipping!')
-            continue
-        
-        out_dir = os.path.join(params['RASTERIZED_POINT_DIR'], tile)
-        
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-        point_out = os.path.join(out_dir, 'point_rasterized.tif')
-        ds=gdal.Open(blue_band_path)
-        extent=GetExtent(ds)
-        rasterize_command = 'gdal_rasterize ' + \
-                            '-a {} '.format(params['TREE_CLASS_COLUM_NAME']) + \
-                            '-ts {pixel_num} {pixel_num} '.format(pixel_num = params['RASTER_PIXEL_NUM']) + \
-                            '-a_nodata {} '.format(params['NO_DATA_OUTPUT']) + \
-                            '-te {} '.format(extent) + \
-                            '-ot Byte ' + \
-                            '-of GTiff {point_path_in} {point_path_out}'.format(point_path_in=params['PURE_POINTS_PATH'], point_path_out=point_out)
-        os.system(rasterize_command)
+# 2. Öffne alle vrts einmal
+datasets = {band: rasterio.open(path) for band, path in vrt_paths.items()}
+n_layers = datasets[bands[0]].count
 
+for _, row in tqdm(gdf.iterrows(), total=len(gdf), desc="Processing samples"):
+    # iterate over every data point:
+    coords = row.geometry
+    spec = row.spec
+    point_data = []
+    for band in bands:
+        # get the raster band vrt
+        dataset = datasets[band]
+        try:
+            values = []
+            row_idx, col_idx = dataset.index(coords.x, coords.y)
+            # get data from bands 1 to 28
+            for i in range(1, n_layers + 1):  # bands 1 to 28
+                v = dataset.read(i, window=Window(col_idx, row_idx, 1, 1))[0, 0]
+                values.append(v)
+            values = np.array(values) # dimension [,28]
+        except Exception:
+            values = np.full(n_layers, np.nan)
+        point_data.append(values) # append all bands
+    sample_array = np.stack(point_data, axis=-1) # becomes an array [28,10]
+    arr_x.append(sample_array)
+    arr_y.append(spec)
+    # 3. Speichern
+    arr_x_out = np.array(arr_x)
+    arr_y_out = np.array(arr_y)
+    np.save(os.path.join('/data/ahsoka/eocp/forestpulse/02_scripts/Synth_Mix/visualization/thermal', "x_arr.npy"), arr_x_out)
+    np.save(os.path.join('/data/ahsoka/eocp/forestpulse/02_scripts/Synth_Mix/visualization/thermal', "y_arr.npy"), arr_y_out)
+
+# Schließe Raster-Dateien
+for ds in datasets.values():
+    ds.close()
+
+# 3. Speichern
+arr_x = np.array(arr_x)
+arr_y = np.array(arr_y)
+print(arr_x.shape)
+print(arr_x[173,:,0])
+print(arr_x[173,:,2])
+print(arr_x[173,:,7])
+print(arr_y.shape)
+np.save(os.path.join('/data/ahsoka/eocp/forestpulse/02_scripts/Synth_Mix/visualization/thermal', "x_arr.npy"), arr_x)
+np.save(os.path.join('/data/ahsoka/eocp/forestpulse/02_scripts/Synth_Mix/visualization/thermal', "y_arr.npy"), arr_y)
