42e5b5adb576c225685f570026f1b0b6668fcf71
davidklehr <klehr@uni-trier.de>
2025-05-28 13:40:18 +0200

    script 1 prepared for paralelization

--- src/1_extract_pure.py
+++ src/1_extract_pure.py
@@ -14,56 +14,58 @@ parser.add_argument("--year", help="path to the file of the training points geop
 parser.add_argument("--working_directory", help="path to the file of the training points geopackage", default= "/data/ahsoka/eocp/forestpulse/01_data/02_processed_data/Synth_Mix/2021_ThermalTime")
 args = parser.parse_args()
 
-
-bands = ['BLU', 'GRN', 'RED', 'RE1', 'RE2', 'RE3', 'BNIR', 'NIR', 'SW1', 'SW2']
-vrt_paths = {
-    band: os.path.join(args.dc_folder, f"mosaic/stack_{band}.vrt")
-    for band in bands
-}
-
-if not os.path.exists( os.path.join(args.working_directory, '1_pure')):
-    os.makedirs(os.path.join(args.working_directory, '1_pure'))
-    
-# 1. load point data
-print(args.training_points)
-gdf = gpd.read_file(args.training_points)
-gdf = gdf.to_crs("EPSG:3035")
-
-arr_x = []
-arr_y = []
-
-# 2. opan all vrts
-datasets = {band: rasterio.open(path) for band, path in vrt_paths.items()}
-n_layers = datasets[bands[0]].count
-
-for _, row in tqdm(gdf.iterrows(), total=len(gdf), desc="Processing samples"):
-    # iterate over every data point:
-    coords = row.geometry
-    spec = row.spec
-    point_data = []
+def extract_points(tile):
+    dc_path = os.path.join(args.dc_folder, tile)
+    bands = ['BLU', 'GRN', 'RED', 'RE1', 'RE2', 'RE3', 'BNIR', 'NIR', 'SW1', 'SW2']
+    data_by_band = {}
     for band in bands:
-        # get the raster band vrt
-        dataset = datasets[band]
-        try:
-            values = []
-            row_idx, col_idx = dataset.index(coords.x, coords.y)
-            # get data from bands 1 to 28
-            for i in range(1, n_layers + 1):  # bands 1 to 28
-                v = dataset.read(i, window=Window(col_idx, row_idx, 1, 1))[0, 0]
-                values.append(v)
-            values = np.array(values) # dimension [,28]
-        except Exception:
-            values = np.full(n_layers, np.nan)
-        point_data.append(values) # append all bands
-    sample_array = np.stack(point_data, axis=-1) # becomes an array [28,10]
-    arr_x.append(sample_array)
-    arr_y.append(spec)
-    # 3. save (iteration-wise)
-    arr_x_out = np.array(arr_x)
-    arr_y_out = np.array(arr_y)
-    np.save(os.path.join(args.working_directory, '1_pure' , f"x_{str(args.year)}.npy"), arr_x_out)
-    np.save(os.path.join(args.working_directory, '1_pure' , f"y_{str(args.year)}.npy"), arr_y_out)
+        band_paths =[]
+        for datei in os.listdir(dc_path):
+            if ('_'+band+'_' in datei) and datei.endswith('.tif'):
+                path = os.path.join(dc_path, datei)
+                band_paths.append(path)   
+        band_paths = sorted(band_paths)
+        band_data = [rasterio.open(path) for path in band_paths]
+        data_by_band[band] = band_data
+        
+    # 1. load point data
+    gdf = gpd.read_file(args.training_points)
+    gdf = gdf.to_crs("EPSG:3035")
 
-# close Raster-data
-for ds in datasets.values():
-    ds.close()
+    # 2. opan all vrts
+    datasets = {band: rasterio.open(path) for band, path in vrt_paths.items()}
+    n_layers = datasets[bands[0]].count
+
+    #for fid, row in tqdm(gdf.iterrows(), total=len(gdf), desc="Processing samples"):
+    for fid, row in gdf.iterrows():
+        # iterate over every data point:
+        coords = row.geometry
+        spec = row.spec
+        point_data = []
+        for band in bands:
+            # get the raster band
+            all_dates = data_by_band[band]
+            try:
+                values = []
+                row_idx, col_idx = all_dates[0].index(coords.x, coords.y)
+                # get data from bands 1 to 28
+                for data_at_date in all_dates:   # bands 1 to 28
+                    v = data_at_date.read(1, window=Window(col_idx, row_idx, 1, 1))[0, 0]
+                    values.append(v)
+                values = np.array(values) # dimension [,28]
+            except Exception:
+                values = np.full(n_layers, np.nan)
+            point_data.append(values) # append all bands
+        sample_array = np.stack(point_data, axis=-1) # becomes an array [28,10]
+        if not np.isnan(sample_array).all():
+            np.savetxt(os.path.join(args.working_directory, '1_pure', f'samples_x{str(args.year)}',f'x_{str(fid).zfill(4)}.csv'), sample_array, delimiter=",", fmt="%d")
+            np.savetxt(os.path.join(args.working_directory, '1_pure', f'samples_y{str(args.year)}',f'y_{str(fid).zfill(4)}.csv'), [np.array(spec)], fmt="%d")
+
+    # close Raster-data
+    for band in bands:
+        all_dates = data_by_band[band]
+        for data_at_date in all_dates: 
+            data_at_date.close()
+
+if __name__ == '__main__':
+    extract_points(tile)
